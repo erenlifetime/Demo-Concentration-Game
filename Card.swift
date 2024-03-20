42153a68ac4242fed04ab6bac10ec8eb413a0490
Eren Pekdemir <93570359+erenlifetime@users.noreply.github.com>
2024-03-20 16:02:22 +0300

    Card.swift

--- Card.swift
+++ Card.swift
@@ -1,14 +1,24 @@
-struct Card{
-  var isFaceUp = false
-  var isMatched = false
-  var identifier:Int
-  static var identifierFactory = 0
-  static func allFactory()->Int{
-    identifierFactory += 1
-    return identifierFactory
-  }
-  init(){
-    self.identifier = Card.allFactory
-  }
-  }
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Eren lifetime on 15.03.2024.
+import Foundation
+struct Card: Hashable{
+    var hashValue: Int { return identifier }
+    static func == (lhs: Card,rhs: Card) -> Bool{
+        return lhs.identifier == rhs.identifier
+    }
+    var isMatched = false
+    var isFaceUp = false
+    private var identifier:Int
+    private static var identifierFactory = 0
+    private static func getUniqueIdentifier() -> Int{
+        identifierFactory += 1
+        return identifierFactory
+    }
+    init(){
+        self.identifier = Card.getUniqueIdentifier()
+    }
 }
+
