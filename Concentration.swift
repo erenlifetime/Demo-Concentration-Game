956c8495bae324e54c28dc77c1e8cda9fc3a9bb2
Eren Pekdemir <93570359+erenlifetime@users.noreply.github.com>
2024-03-20 16:02:00 +0300

    Concentration.swift

--- Concentration.swift
+++ Concentration.swift
@@ -1,46 +1,53 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Eren lifetime on 15.03.2024.
+//
+import UIKit
 class Concentration{
-  var cards = [Card]()
-  var indexofOneAndOnlyFaceUpcard : Int? {
-    get{
-      var foundIndex : Int?
-      for index in cards.indices{
-        if cards[index].isFaceUp{
-          if foundIndex == nil{
-            foundIndex = index
-          }else{
-            return nil
-          }
+    private (set) var cards = [Card]()
+    var cardsAll = [Card]()
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get{
+            return cards.indices.filter{cards[$0].isFaceUp}.oneAndOnly
+        }set{
+            for index in cards.indices{
+                cards[index].isFaceUp = (index == newValue)
+            }
         }
-      }
-      return index
-    }set{
-      for index in cards.indices{
-         cards[index].isFaceUp = (index == newValue)
-        }
-      }
     }
-  func chooseCard(at index:Int){
-    if !cards[index].isMatched{
-      if let matchIndex = indexofOneAndOnlyFaceUpcard , matchIndex != index{
-        if cards[matchIndex].identifier == cards[index].identifier{
-          cards[matchIndex].isMatched = true
-          cards[index].isMatched = true
-          indexofOneAndOnlyFaceUpcard = nil
+    func chooseCard(at index: Int){
+       assert(cards.indices.contains(index)," Concentration.chooseCard(at: \(index)): choosen index not in the cards")
+        if !cards[index].isMatched{
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
+                if cards[matchIndex] == cards[index] {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            }else{
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices{
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
-        cards[index].isFaceUp = true
-      }else{
-        // either no cards or 2 cards are face up (kartları döndürürsün)
-          for flipDownIndex in cards.indices{
-            cards[flipDownIndex].isFaceUp = false
-          }
-        cards[index].isFaceUp = true
-        indexofOneAndOnlyFaceUpcard = index
+    }
+    init(cardCiftiSayisi:Int){
+        assert(cardCiftiSayisi > 0,"Concentration.init \(cardCiftiSayisi)) : you must have least one pair of cards")
+        for _ in 1...cardCiftiSayisi{
+            let card = Card()
+            cards += [card, card]
         }
-      }
+        // ToDo : Shuffle the cards
     }
-  init(cardCiftiSayisi:Int){
-    for _ in 1...cardCiftiSayisi{
-      cards += (card , card)
-    }
-  }
 }
+extension Collection{
+    var oneAndOnly : Element?{
+        return count == 1 ? first : nil
+        }
+
+    }
